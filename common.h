fba74e6d21481d290747c534881a6cb2f5a87a4b
Tatsuya Ishikawa <c611701441@gmail.com>
2018-10-31 17:35:48 +0900

    ヘッダファイル

---
+++ common.h
@@ -0,0 +1,28 @@
+/*****************************************************************
+�ե�����̾	: common.h
+��ǽ		: �����С��ȥ��饤����Ȥǻ��Ѥ������������Ԥ�
+*****************************************************************/
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<assert.h>
+#include<math.h>
+
+#define PORT			(u_short)8888	/* �ݡ����ֹ� */
+
+#define MAX_CLIENTS		4				/* ���饤����ȿ�κ����� */
+#define MAX_NAME_SIZE	10 				/* �桼����̾�κ�����*/
+
+#define MAX_DATA		200				/* ����������ǡ����κ����� */
+
+#define END_COMMAND		'E'		  		/* �ץ���ཪλ���ޥ�� */
+
+
+extern int clientID;
+
+#endif
