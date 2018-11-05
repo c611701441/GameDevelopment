59e6fb1760ab556555a58bbd3da3de43de23a8e7
kitano <c611701381@tokushima-u.ac.jp>
2018-11-05 16:56:38 +0900

    文字化けを修正、キャラクターの構造体、マップの定義を追加

--- common.h
+++ common.h
@@ -1,6 +1,6 @@
 /*****************************************************************
-�ե�����̾	: common.h
-��ǽ		: �����С��ȥ��饤����Ȥǻ��Ѥ������������Ԥ�
+ファイル名	: common.h
+機能		: サーバーとクライアントで使用する定数の宣言を行う
 *****************************************************************/
 
 #ifndef _COMMON_H_
@@ -13,16 +13,31 @@
 #include<assert.h>
 #include<math.h>
 
-#define PORT			(u_short)8888	/* �ݡ����ֹ� */
+#define PORT			(u_short)8888	/* ポート番号*/
 
-#define MAX_CLIENTS		4				/* ���饤����ȿ�κ����� */
-#define MAX_NAME_SIZE	10 				/* �桼����̾�κ�����*/
+#define MAX_CLIENTS		4				/* クライアントの最大数 */
+#define MAX_NAME_SIZE	10 				/* ユーザー名の最大数*/
 
-#define MAX_DATA		200				/* ����������ǡ����κ����� */
+#define MAX_DATA		200				/* 送受信するデータの最大数*/
 
-#define END_COMMAND		'E'		  		/* �ץ���ཪλ���ޥ�� */
+#define END_COMMAND		'E'		  		/* プログラム終了コマンド*/
 
 
 extern int clientID;
+
+/*キャラクターの構造体*/
+typedef struct {
+    int hp; /*体力*/
+    int at; /*攻撃力*/
+    int item; /*所持アイテムの番号*/
+    int key; /*鍵を持っているかどうか*/
+    SDL_rect rect; /*座標*/
+}Character;
+
+/*マップサイズ*/
+enum {
+    MAP_width = 4000,/*横幅*/
+    MAP_high = 2800,/*高さ*/
+};
 
 #endif
