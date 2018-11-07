44362b7e4901f3a163977427c5edeec0ce0cf764
kitano <c611701381@tokushima-u.ac.jp>
2018-11-07 15:06:09 +0900

    キャラクターの初期設定、変数作成clinent_main.c

--- client_main.c
+++ client_main.c
@@ -5,7 +5,17 @@
 
 #include"common.h"
 #include"client_func.h"
+
+/*キャラクターのグローバル変数*/
+extern Character player[3];
+extern Character enemy;
+
 int clientID;
+
+static void SetChara( void );
+static void SetPoint( void );
+
+
 int main(int argc,char *argv[])
 {
     int		num;
@@ -38,6 +48,9 @@ int main(int argc,char *argv[])
 		return -1;
 	}
 
+        /*キャラクターの初期設定*/
+        SetChara();
+        
     /*メインイベントループ*/
     while(endFlag){
 		WindowEvent(num);
@@ -49,4 +62,49 @@ int main(int argc,char *argv[])
 	CloseSoc();
 
     return 0;
+}
+
+/********************************************
+関数名　: SetChara
+機能　　: キャラクターの初期設定
+鬼のクライアントIDを3と仮定しました。
+ ********************************************/
+void SetChara(void)
+{
+    int i;
+    
+    for( i = 0 ; i < 3 ; i++ )
+    {
+        player[i].hp = 1;
+        player[i].at = 1;
+        player[i].item = 0;/*アイテムを所持していないとき0*/
+        player[i].key = 0;/*鍵を持っていないとき0*/
+    }
+    
+    enemy.hp = 100;
+    enemy.at = 300;
+    enemy.item = 0;
+    enemy.key = 0;
+
+    SetPoint();/*自分の座標を設定*/
+}
+
+/********************************************
+関数名　: SetPoint
+機能　　: 自分のキャラクターの初期座標を設定
+********************************************/
+void SetPoint(void)
+{
+    int x , y;
+
+    if(clientID < 3){
+    x = 100;/*適当に書きました。あとでランダムにしましょう。*/
+    y = 100;
+    
+    player[clientID].rect = { x , y };
+    
+    }else{
+     enemy.rect = { 2000 , 1000 };/*鬼の初期位置。今は適当に書きました*/
+    }
+     
 }
